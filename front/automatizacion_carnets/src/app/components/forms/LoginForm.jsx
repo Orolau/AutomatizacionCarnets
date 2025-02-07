c6a83477e85d56ba209cdc26e513332156fd1399
POLSEN006 <114981985+POLSEN006@users.noreply.github.com>
2025-02-07 12:41:59 +0100

    inicios login
    restructuración de carpetas components, pages y styles, añadido el código base de login

---
+++ front/automatizacion_carnets/src/app/components/forms/LoginForm.jsx
@@ -0,0 +1,68 @@
+"use client"
+
+import { useState } from "react";
+import Link from "next/link";
+import {Formik, Form, Field, ErrorMessage} from 'formik';
+import * as Yup from 'yup';
+import '@/app/styles/LoginForm.css'
+import {useRouter} from "next/navigation";
+
+const validationSchema = Yup.object().shape({
+    email: Yup.string().required('Este campo es obligatorio').email('EL formato es incorrecto'),
+    password: Yup.string().required('Este campo es obligatorio')
+});
+
+export default function LoginForm(){
+    const router = useRouter();
+
+    const[userInfo, setUserInfo] = useState({
+        email:'',
+        password:''
+    });
+
+    const handleSubmit = async(values) =>{
+
+        //const respuestaServer = await LoginInteract(values); 
+        //console.log(respuestaServer);
+        console.log(values);
+        router.push('/pages/userForms/verify');
+        /*if(respuestaServer.token !== null){
+            localStorage.setItem('tokenLogin', respuestaServer.token);
+            router.push('/dashboard/clientes/create');
+        }*/
+    }
+
+    return (
+        <div className="contenedorLogin bg-white flex-wrap justify-items-center p-20 shadow-md border rounded-lg">
+            <Formik initialValues={userInfo} validationSchema={validationSchema} onSubmit={handleSubmit}>
+                {()=>(
+                    <Form className="flex-wrap">
+                        <h1 className="titulo font-bold mb-5">Login to your account</h1>
+                        <div>
+                            <Field name="email" type="email" placeholder="email" className="inputLogin border rounded-md"/>
+                            <ErrorMessage name="email" component="div" className="error"/>
+                        </div>
+                        <div>
+                            <div className="passwordContainer">
+                                <Field name="password" type="password" placeholder="password" className="inputLogin border rounded-md" id="passWordField"></Field>
+                                <button className="viewPassword">
+                                    <img
+                                        src="https://img.icons8.com/material-outlined/24/000000/invisible.png"
+                                        className="size-5"
+                                    />
+                                </button>
+                            </div>
+                            <ErrorMessage name="password" component="div" className="error"/>
+                        </div>
+                        <button type="submit" className="bg-blue-300 w-[300px] border border-cyan-500 rounded-lg mb-5 hover:bg-blue-400">Sing in with email</button>
+                        <p className="dontHaveAccount ml-12 text-gray-500">
+                            Don´t have an account?
+                            <Link href="/user/register" className="getStartedButton font-bold">
+                                Get Started
+                            </Link>
+                        </p>
+                    </Form>
+                )}
+            </Formik>
+        </div>);
+}
---
+++ front/automatizacion_carnets/src/app/pages/userForms/verify/page.jsx
@@ -0,0 +1,7 @@
+"use client"
+
+export default function VerifyPage(){
+    return(
+        <p>Hola</p>
+    );
+}
