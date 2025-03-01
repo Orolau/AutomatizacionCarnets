1df31b1db83dcfeaa0a56418b6d4593b72eaa711
jaimecc04 <jamescc2004@gmail.com>
2025-03-01 19:23:07 +0100

    Obtener carnets, hechos y pendientes, solo hechos y cambiar de pendientes a hechos

--- back/models/index.js
+++ back/models/index.js
@@ -1,6 +1,7 @@
 const models = {
     userModel: require('./user.js'),
-    personModel: require('./person.js')
+    personModel: require('./person.js'),
+    estadoModel: require('./estado.js'),
 }
 
 module.exports = models;
