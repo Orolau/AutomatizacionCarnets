0af8feb179d3eae32e12727a79ce7b1b9fca8d4e
alexCaballero4 <alexcaballero846@gmail.com>
2025-03-24 19:59:04 +0100

    Pruebas Diseño

---
+++ front/automatizacion_carnets/src/app/pages/finales/principal/page.jsx
@@ -0,0 +1,8 @@
+import FilterPersonalDataForm from '@/app/components/forms/FilterPersonalDataForm';
+
+
+export default function PrincipalPage() {
+  return (
+    <FilterPersonalDataForm />
+  );
+}
